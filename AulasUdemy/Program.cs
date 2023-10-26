a3e01442a1b20180fbb8726f9ac1ecdbd104c2c2
Samuel Bozio <samuel@rodosoft.com.br>
2023-10-26 08:44:42 -0300

    multicast delegates, a combinacao de delegates

---
+++ AulasUdemy/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+    delegate void OperacoesNumericas(double n1, double n2);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double a = 10;
+        double b = 20;
+
+        OperacoesNumericas on = CalculationService.ShowSum;
+        on += CalculationService.ShowMax;
+        on.Invoke(a, b);   
+        
+
+
+    }
+}
+
+
+
+
+
+class CalculationService 
+{
+
+    public static void ShowMax(double x, double y)
+    {
+        double max = (x > y)  ? x : y;  
+        Console.WriteLine(max);
+    }
+
+    public static void ShowSum(double x, double y)
+    {
+        double sum = x + y;
+        Console.WriteLine(sum);
+    }
+
+
+
+}
+
+
+
